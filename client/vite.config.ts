d6f12ee848188ca06ad9c781da64873ef25528dd
Manolo <newyozhy@gmail.com>
2025-03-09 18:52:39 -0500

    Acceso al Frontend HTTPS

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Permite el acceso desde otros dispositivos
+    host: true,
+    allowedHosts: ['inventario-chasu.duckdns.org']
   },
 });
